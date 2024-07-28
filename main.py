b5573604b6b6f85fde358cbba96873ef531c0a1f
EvanB002 <embradfo@mtu.edu>
2024-07-28 15:32:34 -0400

    Test commit to make sure everything is working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print("Welcome to high sea merchants! A game of risk and reward, of supply and d
       " to make a fortune out on the high seas? Do you have\nwhat it takes to become the best merchant in the world? Find out now!\n")
 
 # Testing Stuff Below
-player = Player('Test', 500, 100.00, None, 53, 44)
+player = Player('Test', 300, 100.00, None, 53, 44)
 island = Island('Beehive', 100000, 500000.00, None, 53, 44)
 
 player.playerInfo()
