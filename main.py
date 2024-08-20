d332eb5b9257586df6eff29a71998fcb6e78b8ad
EvanB002 <embradfo@mtu.edu>
2024-08-20 12:38:29 -0400

    Title Screen
    Began work on title screen, also changed name standards so all image assets have "HSM" in the name for "High Seas Merchants"

--- main.py
+++ main.py
@@ -19,21 +19,27 @@ player = Player('Joshua', 300, 100.00, Inventory(True, globalItemList), 7, 13)
 
 # Pygame Setup
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 
 pygame.display.set_caption("High Seas Merchants")
-icon = pygame.image.load('Assets/BoatIcon2.png')
+icon = pygame.image.load('Assets/IconHSM.png')
 pygame.display.set_icon(icon)
+
+titleScreen = pygame.image.load('Assets/TitleScreenHSM.png')
+screen.fill(color=(153, 204, 255))
+pygame.Surface.blit(screen, titleScreen, (500, 200))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    pygame.display.flip()
+    clock.tick(60)
 
-
+pygame.quit()
 
 # Testing Below
 player.inventory.addItem(globalItemList[2], 5)
