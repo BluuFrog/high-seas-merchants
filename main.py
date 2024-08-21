1a2e4e1abd2b793f87faa72108451c0a5766b79d
EvanB002 <embradfo@mtu.edu>
2024-08-21 12:20:35 -0400

    Title Screen Finished
    Updated the title screen image, added text asking the player to press any key to continue, also labeled setup section of main.py with more inline comments

--- main.py
+++ main.py
@@ -18,20 +18,23 @@ setupObject.openingText()
 player = Player('Joshua', 300, 100.00, Inventory(True, globalItemList), 7, 13)
 
 # Pygame Setup
-pygame.init()
+pygame.init() # Initial setup
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 
-pygame.display.set_caption("High Seas Merchants")
+pygame.display.set_caption("High Seas Merchants") # Window name and icon
 icon = pygame.image.load('Assets/IconHSM.png')
 pygame.display.set_icon(icon)
 
-titleScreen = pygame.image.load('Assets/TitleScreenHSM.png')
+titleScreen = pygame.image.load('Assets/TitleScreenHSM.png') # Title screen
 screen.fill(color=(153, 204, 255))
-pygame.Surface.blit(screen, titleScreen, (500, 200))
+font = pygame.font.SysFont("constantia", 34, False, False)
+text = font.render("Press any key to continue", True, pygame.Color((115, 62, 57)))
+pygame.Surface.blit(screen, titleScreen, (500, 125))
+pygame.Surface.blit(screen, text, (570, 600))
 
-while running:
+while running: # Game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -48,3 +51,5 @@ player.inventory.addItem(globalItemList[17], 3)
 player.inventory.removeItem(globalItemList[8], 5)
 player.trade(globalItemList[4], 7, globalItemList[2], 4, 200)
 player.inventory.showItems()
+
+#print(pygame.font.get_fonts())
